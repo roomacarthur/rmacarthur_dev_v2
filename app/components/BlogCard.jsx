d64bf7e979ea1f55813f08dc91e7c82f0e6718e5
roomacarthur <roomacarthur@gmail.com>
2023-11-27 11:59:35 +0000

    Updated Styling additional styling to blog card and sidebar within blogdetail page

--- app/components/BlogCard.jsx
+++ app/components/BlogCard.jsx
@@ -13,7 +13,7 @@ export default function BlogCard({ blog }) {
   }
   return (
     <Link href={`blog/${blog.slug}`} className="w-full">
-      <div className="bg-gray-800/80 w-full mt-4 text-foreground hover:bg-foreground/10 p-4 hover:shadow-lg hover:backdrop-blur-lg rounded-lg hover:border-t border-foreground/60 border-t shadow-lg border-foreground/20">
+      <div className=" w-full mt-4 text-foreground hover:bg-foreground/10 p-4 border-1 rounded-lg shadow-lg">
         <h2 className="tracking-wide underline">{blog.title}</h2>
         <p className="text-left py-4">{`${blog.snippet}`}</p>
         <p className="text-xs font-light text-gray-200 pb-4">
