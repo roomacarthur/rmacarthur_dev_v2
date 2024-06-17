4495f16048a603fd2e24c6e42e85f961cb856ca3
roomacarthur <roomacarthur@gmail.com>
2024-06-17 20:51:17 +0100

    Updates BlogCard
    - new nextui card with styling

--- app/components/BlogCard.jsx
+++ app/components/BlogCard.jsx
@@ -1,6 +1,11 @@
-import { Card } from "@nextui-org/card";
+import { Card, CardFooter, CardBody, Image } from "@nextui-org/react";
 import { Link } from "@nextui-org/link";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function BlogCard({ blog, categoriesMap }) {
   function formatDate(dateString) {
@@ -8,27 +13,41 @@ export default function BlogCard({ blog, categoriesMap }) {
     const day = date.getDate();
     const month = date.toLocaleString("default", { month: "short" });
     const year = date.getFullYear().toString().substr(-2);
-
     return `${day} ${month} ${year}`;
   }
-
   return (
     <Link href={`blog/${blog.slug}`} className="w-full h-full">
-      <Card className="w-full h-full text-foreground bg-gray-800/70 p-4 rounded-lg shadow-lg flex flex-col">
-        <h2 className="tracking-wide underline">{blog.title}</h2>
-        <p className="text-xs font-light text-gray-200 pb-4 mt-2">
-          {`Published: ${formatDate(blog.published_date)}`}
-        </p>
-        <div className="mt-auto">
-          {blog.categories.map((categoryId, index) => (
-            <span
-              key={index}
-              className="text-[0.5rem] leading-[0.75rem] tracking-wide me-2 px-1.5 py-0.5 rounded-sm bg-teal-100/10 shadow-md cursor-pointer text-foreground hover:text-teal-300"
-            >
-              {categoriesMap[categoryId]}
-            </span>
-          ))}
-        </div>
+      <Card
+        shadow="md"
+        isPressable
+        className="bg-gray-800/70 h-full flex flex-col justify-items-start"
+      >
+        <CardBody id="CardBody" className="p-0 flex flex-col max-h-[200px]">
+          <Image
+            id="CardImage"
+            shadow="sm"
+            radius="lg"
+            width="100%"
+            alt={blog.title}
+            className="w-full flex object-cover h-[200px]"
+            src={blog.thumbnail}
+          />
+        </CardBody>
+        <CardFooter id="CardFooter" className="p-4 pt-2 flex">
+          <div className="mb-2">
+            <div className="flex space-x-4 text-[0.6rem] text-muted mb-2">
+              <span className="flex items-center">
+                <FontAwesomeIcon icon={faCalendarDays} className="mr-1" />
+                {formatDate(blog.published_date)}
+              </span>
+              <span className="flex items-center">
+                <FontAwesomeIcon icon={faClock} className="mr-1" />
+                {`${blog.read_time} mins`}
+              </span>
+            </div>
+            <h2 className="text-lg tracking-md font-semibold text-left">{blog.title}</h2>
+          </div>
+        </CardFooter>
       </Card>
     </Link>
   );
