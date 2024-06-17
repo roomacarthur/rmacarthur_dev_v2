a0ce25b97a6dcf5ee989fce9897da61b1ef4f28d
roomacarthur <roomacarthur@gmail.com>
2024-06-17 12:15:36 +0100

    Updates BlogCard
    - styled to keep cards the same size
    - styled component badges to suit

--- app/components/BlogCard.jsx
+++ app/components/BlogCard.jsx
@@ -1,7 +1,6 @@
 import { Card } from "@nextui-org/card";
 import { Link } from "@nextui-org/link";
 import React from "react";
-// ... (other imports)
 
 export default function BlogCard({ blog, categoriesMap }) {
   function formatDate(dateString) {
@@ -14,17 +13,17 @@ export default function BlogCard({ blog, categoriesMap }) {
   }
 
   return (
-    <Link href={`blog/${blog.slug}`} className="w-full">
-      <Card className="w-full mt-4 text-foreground bg-gray-800/70 p-4 rounded-lg shadow-lg">
+    <Link href={`blog/${blog.slug}`} className="w-full h-full">
+      <Card className="w-full h-full text-foreground bg-gray-800/70 p-4 rounded-lg shadow-lg flex flex-col">
         <h2 className="tracking-wide underline">{blog.title}</h2>
         <p className="text-xs font-light text-gray-200 pb-4 mt-2">
           {`Published: ${formatDate(blog.published_date)}`}
         </p>
-        <div>
+        <div className="mt-auto">
           {blog.categories.map((categoryId, index) => (
             <span
               key={index}
-              className="inline-block mr-2 mb-2 px-4 py-1 rounded-full text-xs font-bold bg-teal-400/10 shadow-md cursor-default text-teal-300"
+              className="text-[0.5rem] leading-[0.75rem] tracking-wide me-2 px-1.5 py-0.5 rounded-sm bg-teal-100/10 shadow-md cursor-pointer text-foreground hover:text-teal-300"
             >
               {categoriesMap[categoryId]}
             </span>
